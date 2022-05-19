44116b08ee97ada3efdb82b7a5a5f1c4e3c77cc0
kamatozXIII <kamatozXIII@gmail.com>
2022-05-19 14:30:10 +0300

    add

--- Giant - Best Free Bootstrap Coming Soon template _ webthemez_files/main.js
+++ Giant - Best Free Bootstrap Coming Soon template _ webthemez_files/main.js
@@ -1,15 +1,14 @@
 
-jQuery(document).ready(function() {	
-	
+jQuery(document).ready(function() {
+
     /*
         Background slideshow
     */
 	$('.banner-area').backstretch([
 	                     "images/backgrounds/1.jpg"
 	                   , "images/backgrounds/2.jpg"
-	                   , "images/backgrounds/3.jpg"
 	                  ], {duration: 3000, fade: 750});
-	
+
 
 	$("#typed").typed({
 		// strings: ["Typed.js is a <strong>jQuery</strong> plugin.", "It <em>types</em> out sentences.", "And then deletes them.", "Try it out!"],
@@ -27,13 +26,13 @@ jQuery(document).ready(function() {
 	$(".reset").click(function(){
 		$("#typed").typed('reset');
 	});
- 
- 
+
+
     function newTyped(){ /* A new typed object */ }
 
     function foo(){ console.log("Callback"); }
 
-	
+
 });
 
 // cowntdown function. Set the date below (December 1, 2016 00:00:00):
@@ -41,8 +40,8 @@ var austDay = new Date("August 22, 2018 00:00:00");
 
 	$('#countdown').countdown({until: austDay, layout: '<div class="item"><p>{dn}</p> {dl}</div> <div class="item"><p>{hn}</p> {hl}</div> <div class="item"><p>{mn}</p> {ml}</div> <div class="item"><p>{sn}</p> {sl}</div>'});
 	$('#year').text(austDay.getFullYear());
-	
-// smooth scrolling	
+
+// smooth scrolling
 	$(function() {
   $('a[href*=#]:not([href=#])').click(function() {
 	if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
