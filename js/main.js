a4e5021a97cf0ed60ca8c7ee92373c6db3318193
kamatozXIII <kamatozXIII@gmail.com>
2022-05-23 16:03:56 +0300

    add

--- js/main.js
+++ js/main.js
@@ -5,8 +5,11 @@ jQuery(document).ready(function() {
         Background slideshow
     */
     $('.banner-area').backstretch([
-        "images/backgrounds/1.jpg"
-        , "images/backgrounds/2.jpg"
+        "images/backgrounds/1.jpg",
+        "images/backgrounds/2.jpg",
+        "images/backgrounds/3.jpg",
+        "images/backgrounds/4.jpg",
+        "images/backgrounds/5.jpg"
     ], {duration: 5000, fade: 750});
 
 
